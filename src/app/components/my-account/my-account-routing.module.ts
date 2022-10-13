4ce1bb3ffe40d4145c7b1d1db5933705e7b1e4dc
nguyenvantuanDEV <110893485+tuan2910@users.noreply.github.com>
2022-10-13 23:29:14 +0700

    update base project

--- src/app/components/my-account/my-account-routing.module.ts
+++ src/app/components/my-account/my-account-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountComponent } from './account/account.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-import { MyAddressComponent } from './my-address/my-address.component';
 
 const routes: Routes = [
-  { path: 'profile', component: AccountComponent },
-  { path: 'change-password',component: ChangePasswordComponent},
-  { path: 'address', component: MyAddressComponent },
+  // { path: 'profile', component: AccountComponent },
+  // { path: 'change-password',component: ChangePasswordComponent},
+  // { path: 'address', component: MyAddressComponent },
 ];
 
 @NgModule({
--- src/app/components/my-account/my-account.module.ts
+++ src/app/components/my-account/my-account.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MyAccountRoutingModule } from './my-account-routing.module';
-import { AccountComponent } from './account/account.component';
-import { MyAddressComponent } from './my-address/my-address.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MyAccountComponent } from './my-account.component';
 import { NgxPaginationModule } from 'ngx-pagination';
@@ -13,9 +10,6 @@ import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   declarations: [
     MyAccountComponent,
-    AccountComponent,
-    MyAddressComponent,
-    ChangePasswordComponent,
   ],
   imports: [
     CommonModule,
