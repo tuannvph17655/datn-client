86159830adb5d26b585701f54fd3b3a4bbfa687c
nguyenvantuanDEV <110893485+tuan2910@users.noreply.github.com>
2022-10-16 01:20:01 +0700

    code layout account

--- src/app/components/my-account/my-account-routing.module.ts
+++ src/app/components/my-account/my-account-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
-  // { path: 'profile', component: AccountComponent },
+  { path: 'profile', component: AccountComponent },
   // { path: 'change-password',component: ChangePasswordComponent},
   // { path: 'address', component: MyAddressComponent },
 ];
