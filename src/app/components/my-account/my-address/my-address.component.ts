8cad296dc6912564f375a761e4fd58d25ff4bcf0
nguyenvantuanDEV <110893485+tuan2910@users.noreply.github.com>
2022-10-16 01:45:03 +0700

    generate module

--- src/app/components/my-account/my-address/my-address.component.ts
+++ src/app/components/my-account/my-address/my-address.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Address } from 'src/app/models/address';
+import { District } from 'src/app/models/district';
+import { Province } from 'src/app/models/province';
+import { Ward } from 'src/app/models/ward';
 
 @Component({
   selector: 'app-my-address',
@@ -6,7 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-address.component.css']
 })
 export class MyAddressComponent implements OnInit {
+  provinces: Province[] = [];
+  districts : District[] = [];
+  wards: Ward[] = [];
+  provinceSelected !: any;
+  districtSelected !: any;
+  wardCodeSelected !: string;
 
+  @ViewChild('exampleModal') modal:any;
+
+  public address: Address[] = [];
+
+  provinceNameSelected!:string;
+  districtNameSelected!:string;
+  wardNameSelected!:string;
   constructor() { }
 
   ngOnInit(): void {
